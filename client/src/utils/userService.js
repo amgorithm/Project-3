8d71e0437a5b52198c855d2df54bc3471c2a75e3
Amal <amal.mukhtar@outlook.com>
2022-10-23 19:42:32 +0100

    Console log and excess code removed

--- client/src/utils/userService.js
+++ client/src/utils/userService.js
@@ -3,22 +3,17 @@ import tokenService from "./tokenService";
 const BASE_URL = "/api/users/"; // Note: Once deployed this should be updated.
 
 function signup(user) {
-  return (
-    fetch(BASE_URL + "signup", {
-      method: "POST",
-      headers: new Headers({ "Content-Type": "application/json" }),
-      body: JSON.stringify(user),
+  return fetch(BASE_URL + "signup", {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    body: JSON.stringify(user),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+      throw new Error("Email already taken!");
     })
-      .then((res) => {
-        if (res.ok) return res.json();
-        // Probably a duplicate email
-        throw new Error("Email already taken!");
-      })
-      // Parameter destructuring!
-      .then(({ token }) => tokenService.setToken(token))
-  );
-  // The above could have been written as
-  //.then((token) => token.token);
+
+    .then(({ token }) => tokenService.setToken(token));
 }
 
 function getUser() {
@@ -36,20 +31,14 @@ function login(creds) {
     body: JSON.stringify(creds),
   })
     .then((res) => {
-      // Valid login if we have a status of 2xx (res.ok)
       if (res.ok) return res.json();
       throw new Error("Bad Credentials!");
     })
     .then(({ token }) => tokenService.setToken(token));
 }
 
-// TODO: Post profile info
-//! dc
 export const updateProfileInfo = async (profileEdit, userID) => {
-  console.log("UserService check ->", profileEdit);
-
   try {
-    console.log("user service check 2");
     const token = tokenService.getToken();
     let res = await fetch(`/api/users/${userID}`, {
       method: "PATCH",
@@ -59,7 +48,6 @@ export const updateProfileInfo = async (profileEdit, userID) => {
       },
       body: JSON.stringify(profileEdit),
     });
-    console.log("res ->", profileEdit);
     return res.json();
   } catch (error) {
     console.log(error);
