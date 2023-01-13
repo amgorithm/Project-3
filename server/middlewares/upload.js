3bb60434a8f2be7c8daa271be0a1bc71a44caf3b
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-01-13 09:34:41 +0300

    going up two levels in multer path

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -6,7 +6,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "/uploads/"));
+    cb(null, path.join(__dirname, "..", "..", "/uploads/"));
   },
 
   filename: function (req, file, cb) {
